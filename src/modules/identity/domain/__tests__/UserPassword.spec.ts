f41bf6aa5b8d45db2da29f5cc400c6a6d9fba916
Mana-One <paolo.manaois@yahoo.fr>
2021-05-19 02:24:17 +0200

    switched to OOP for UserPassword

--- src/modules/identity/domain/__tests__/UserPassword.spec.ts
+++ src/modules/identity/domain/__tests__/UserPassword.spec.ts
@@ -4,98 +4,166 @@ import { UserPassword } from "../UserPassword";
 describe("UserPassword value object", () => {
     describe("creation", () => {
         it("should create a clear password", () => {
-            const password = UserPassword.create("azertyUIOP123$");
-            expect(password).toBeTruthy();
-            expect(password.isHashed).toBe(false);
-            expect(password.value).toBe("azertyUIOP123$");
+            const passwordOrError = UserPassword.createNotHashed("azertyUIOP123$");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isRight()){
+                const password = passwordOrError.value.getValue();
+                const isHashed = passwordOrError.value.isAlreadyHashed();
+                expect(password).toBe("azertyUIOP123$");
+                expect(isHashed).toBe(false);
+            } else {
+                fail("Should be successful");
+            }
         })
 
         it("should create a hashed password", () => {
-            const password = UserPassword.create(
-                "a hashed password",
-                true
-            );
-            expect(password).toBeTruthy();
-            expect(password.isHashed).toBe(true);
-            expect(password.value).toBe("a hashed password");
+            const passwordOrError = UserPassword.createHashed("azertyUIOP123$");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isRight()){
+                const password = passwordOrError.value.getValue();
+                const isHashed = passwordOrError.value.isAlreadyHashed();
+                expect(password).toBe("azertyUIOP123$");
+                expect(isHashed).toBe(true);
+            } else {
+                fail("Should be successful");
+            }
         })
 
         it("should fail if provided password is not hashed and does not have an uppercase char", () => {
-            expect(() => UserPassword.create("azertyuiop123$")).toThrow(IdentityErrors.InvalidPassword);
+            const passwordOrError = UserPassword.createNotHashed("azertyuiop123$");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isLeft()){
+                const error = passwordOrError.value;
+                expect(error).toBe("Invalid length or format for password");
+            } else {
+                fail("Should not be successful");
+            }
         })
 
         it("should fail if provided password is not hashed and does not have a lowercase char", () => {
-            expect(() => UserPassword.create("AZERTYUIOP123$")).toThrow(IdentityErrors.InvalidPassword);
+            const passwordOrError = UserPassword.createNotHashed("AZERTYUIOP123$");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isLeft()){
+                const error = passwordOrError.value;
+                expect(error).toBe("Invalid length or format for password");
+            } else {
+                fail("Should not be successful");
+            }
         })
 
         it("should fail if provided password is not hashed and does not have a number", () => {
-            expect(() => UserPassword.create("azertyUIOP$")).toThrow(IdentityErrors.InvalidPassword);
+            const passwordOrError = UserPassword.createNotHashed("azertyUIOP$");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isLeft()){
+                const error = passwordOrError.value;
+                expect(error).toBe("Invalid length or format for password");
+            } else {
+                fail("Should not be successful");
+            }
         })
 
         it("should fail if provided password is not hashed and does not have a special symbol", () => {
-            expect(() => UserPassword.create("azertyUIOP123")).toThrow(IdentityErrors.InvalidPassword);
+            const passwordOrError = UserPassword.createNotHashed("azertyUIOP123");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isLeft()){
+                const error = passwordOrError.value;
+                expect(error).toBe("Invalid length or format for password");
+            } else {
+                fail("Should not be successful");
+            }
         })
 
         it("should fail if provided password is not hashed and is too short", () => {
-            expect(() => UserPassword.create("aZ1$")).toThrow(IdentityErrors.InvalidPassword);
+            const passwordOrError = UserPassword.createNotHashed("aZ1$");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isLeft()){
+                const error = passwordOrError.value;
+                expect(error).toBe("Invalid length or format for password");
+            } else {
+                fail("Should not be successful");
+            }
         })
 
         it("should fail if provided password is not hashed and is too long", () => {
-            expect(() => UserPassword.create("azertyUIOP123$".repeat(3))).toThrow(IdentityErrors.InvalidPassword);
+            const passwordOrError = UserPassword.createNotHashed("azertyUIOP123$".repeat(3));
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isLeft()){
+                const error = passwordOrError.value;
+                expect(error).toBe("Invalid length or format for password");
+            } else {
+                fail("Should not be successful");
+            }
         })
     })
 
-    describe("hash", () => {
-        it("should return a new password value object if passing a non-hashed UserPassword", async () => {
-            const password = UserPassword.create("azertyUIOP123$");
-            const hashed = await UserPassword.hash(password);
-            expect(hashed).toBeTruthy();
-            expect(hashed.isHashed).toBe(true);
-            expect(hashed.value).toBeTruthy();
+    describe("hashPassword", () => {
+        it("should return a new password if passing a non-hashed UserPassword", async () => {
+            const passwordOrError = UserPassword.createNotHashed("azertyUIOP123$");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isRight()){
+                const password = await passwordOrError.value.hashPassword();
+                expect(password).toBeTruthy();
+                expect(password).not.toBe("azertyUIOP123$");
+            } else {
+                fail("Should be successful");
+            }
         })
 
-        it("should return the same value object if the passed UserPassword is already hashed", async () => {
-            const password = UserPassword.create(
-                "azertyUIOP123$",
-                true
-            );
-            const hashed = await UserPassword.hash(password);
-            expect(hashed === password).toBe(true);
-            expect(hashed).toEqual(password);
+        it("should return the same value if the passed UserPassword is already hashed", async () => {
+            const passwordOrError = UserPassword.createHashed("azertyUIOP123$");
+            expect(passwordOrError).toBeTruthy();
+            if(passwordOrError.isRight()){
+                const password = await passwordOrError.value.hashPassword();
+                expect(password).toBe("azertyUIOP123$");
+            } else {
+                fail("Should be successful");
+            }
         })
     })
 
-    describe("compare", () => {
+    describe("comparePassword", () => {
         it("should succeed when clearPassword matches UserPassword with hashed value", async () => {
             const clear = "azertyUIOP123$";
-            const hashed = await UserPassword.hash(UserPassword.create("azertyUIOP123$"));
-
-            const check = await UserPassword.compare(clear, hashed);
-            expect(check).toBe(true);
+            const passwordOrError = UserPassword.createHashed("$2a$10$rVr4z.ZT6KUOgZWzJIaqQurwc7gORCtwSQJrcuXJ3.sdRqLYVPWhO");
+            if(passwordOrError.isRight()){
+                const check = await passwordOrError.value.comparePassword(clear);
+                expect(check).toBe(true);
+            } else {
+                fail("Should be successful");
+            }
         })
 
         it("should succeed when clearPassword matches UserPassword with non-hashed value", async () => {
             const clear = "azertyUIOP123$";
-            const notHashed = UserPassword.create("azertyUIOP123$");
-
-            const check = await UserPassword.compare(clear, notHashed);
-            expect(check).toBe(true);
+            const passwordOrError = UserPassword.createNotHashed(clear);
+            if(passwordOrError.isRight()){
+                const check = await passwordOrError.value.comparePassword(clear);
+                expect(check).toBe(true);
+            } else {
+                fail("Should be successful");
+            }
         })
 
         it("should fail when clearPassword does not match UserPassword with hashed value", async () => {
             const clear = "azertyUIOP123$hey";
-            const notHashed = UserPassword.create("azertyUIOP123$");
-
-            const check = await UserPassword.compare(clear, notHashed);
-            expect(check).toBe(false);
+            const passwordOrError = UserPassword.createHashed("$2a$10$rVr4z.ZT6KUOgZWzJIaqQurwc7gORCtwSQJrcuXJ3.sdRqLYVPWhO");
+            if(passwordOrError.isRight()){
+                const check = await passwordOrError.value.comparePassword(clear);
+                expect(check).toBe(false);
+            } else {
+                fail("Should be successful");
+            }
         })
 
         it("should fail when clearPassword does not match UserPassword with non-hashed value", async () => {
             const clear = "azertyUIOP123$hey";
-            const hashed = await UserPassword.hash(UserPassword.create("azertyUIOP123$"));
-
-            const check = await UserPassword.compare(clear, hashed);
-            expect(check).toBe(false);
+            const passwordOrError = UserPassword.createNotHashed("azertyUIOP123$");
+            if(passwordOrError.isRight()){
+                const check = await passwordOrError.value.comparePassword(clear);
+                expect(check).toBe(false);
+            } else {
+                fail("Should be successful");
+            }
         })
     })
 })
