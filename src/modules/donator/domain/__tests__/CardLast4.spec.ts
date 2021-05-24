1ff4dd31f452b6b4e797dc362235f5799298191a
Mana-One <paolo.manaois@yahoo.fr>
2021-05-24 18:56:51 +0200

    added CardLast4 value object

---
+++ src/modules/donator/domain/__tests__/CardLast4.spec.ts
@@ -0,0 +1,33 @@
+import { CardLast4 } from "../CardLast4";
+
+describe("CardLast4 value object", () => {
+    describe("creation", () => {
+        it("should return last4 value object", () => {
+            const value = "1234";
+            const res = CardLast4.create(value);
+            expect(res.success).toBe(true);
+
+            const last4 = res.getValue();
+            expect(last4 instanceof CardLast4).toBe(true);
+            expect(last4.getValue()).toBe(value.padStart(16, "*"));
+        })
+
+        it("should fail when passing an empty string", () => {
+            const res = CardLast4.create("");
+            expect(res.success).toBe(false);
+            expect(res.getValue()).toBe("Invalid last4 value")
+        })
+
+        it("should fail when passing a string that is too long", () => {
+            const res = CardLast4.create("12345");
+            expect(res.success).toBe(false);
+            expect(res.getValue()).toBe("Invalid last4 value")
+        })
+
+        it("should fail when passing an string containing letters", () => {
+            const res = CardLast4.create("12g4");
+            expect(res.success).toBe(false);
+            expect(res.getValue()).toBe("Invalid last4 value")
+        })
+    })
+})
