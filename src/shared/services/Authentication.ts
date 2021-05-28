9e24aa11404ead8a79a66f7b9131291b1df79eb8
Mana-One <paolo.manaois@yahoo.fr>
2021-05-28 12:04:07 +0200

    added JWT authentication service

--- src/shared/services/Authentication.ts
+++ src/shared/services/Authentication.ts
@@ -4,3 +4,9 @@ export interface Authentication {
     createToken(payload: TokenPayload): Promise<AccessToken>;
     verifyAndRetrievePayload(token: AccessToken): Promise<TokenPayload>
 }
+
+export function isTokenPayload(obj: any): obj is TokenPayload {
+    return !!obj === true &&
+        "id" in obj && typeof obj["id"] === "string" &&
+        "role" in obj && obj["role"] === "string";
+}
