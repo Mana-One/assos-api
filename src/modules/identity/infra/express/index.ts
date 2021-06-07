ca16fb9ce6fc8f4aadb0bc19940df9d6eb7121ca
Adem Mrizak <adem.mrizak@gmail.com>
2021-06-07 11:31:12 +0200

    Rename file

--- src/modules/identity/infra/express/index.ts
+++ src/modules/identity/infra/express/index.ts
@@ -1,4 +1,4 @@
 export * from "./change-password.controller";
 export * from "./edit-self.controller";
 export * from "./edit-user.controller";
-export * from "./login.controller.ts";
+export * from "./login.controller";
