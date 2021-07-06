920c6cf4cd93c73a75beb8dcdc0b33da319716bc
Mana-One <paolo.manaois@yahoo.fr>
2021-07-06 15:54:29 +0200

    added Edit Info Usecase

---
+++ src/modules/association/usecases/__tests__/EditInfo.spec.ts
@@ -0,0 +1,110 @@
+import { makeEditInfoUsecase } from '../EditInfo';
+import { FindById, Save } from '../../repositories/__mocks__/AssociationRepo';
+import { AssociationErrors } from '../errors';
+import { AppErrors, Result } from '../../../../core/logic';
+
+
+describe('Edit Presentation Usecase', () => {
+    const props = {
+        associationId: 'an association id',
+        name: 'a new name',
+        email: 'asso@yahoo2.com',
+        bannerUrl: 'https://domain.com/image.png'
+    };
+
+    const deps = {
+        findAssociation: FindById.notNull,
+        save: Save.ok
+    };
+
+    it('should return ok result', async () => {
+        const usecase = makeEditInfoUsecase(deps);
+        const result = await usecase(props);
+        if(result.isRight()){
+            expect(result.value.success).toBe(true)
+        } else {
+            fail('Result should be \'right\'');
+        }
+    })
+
+    it('should return ok result when passing some fields', async () => {
+        const usecase = makeEditInfoUsecase(deps);
+        const result = await usecase({
+            associationId: props.associationId,
+            name: props.name
+        });
+        if(result.isRight()){
+            expect(result.value.success).toBe(true)
+        } else {
+            fail('Result should be \'right\'');
+        }
+    })
+
+    it('should return AssociationNotFound when association does not exist', async () => {
+        const usecase = makeEditInfoUsecase({ 
+            ...deps,
+            findAssociation: FindById.null
+        });
+        const result = await usecase(props);
+        if(result.isLeft()){
+            expect(result.value instanceof AssociationErrors.AssociationNotFound).toBe(true);
+            expect(result.value.success).toBe(false);
+        } else {
+            fail('Result should be \'left\'');
+        }
+    })
+
+    it('should return UnexpectedError when fetching association fails', async () => {
+        const usecase = makeEditInfoUsecase({ 
+            ...deps,
+            findAssociation: FindById.throw
+        });
+        const result = await usecase(props);
+        if(result.isLeft()){
+            expect(result.value instanceof AppErrors.UnexpectedError).toBe(true);
+            expect(result.value.success).toBe(false);
+        } else {
+            fail('Result should be \'left\'');
+        }
+    })
+
+    it('should return ko result when passing an invalid email', async () => {
+        const usecase = makeEditInfoUsecase(deps);
+        const result = await usecase({
+            associationId: props.associationId,
+            email: '.assos@yahoo2.com'
+        });
+        if(result.isLeft()){
+            expect(result.value.success).toBe(false);
+        } else {
+            fail('Result should be \'left\'');
+        }
+    })
+
+    it('should return ko result when passing an invalid name or bannerUrl', async () => {
+        const usecase = makeEditInfoUsecase(deps);
+        const result = await usecase({
+            associationId: props.associationId,
+            name: ''
+        });
+        if(result.isLeft()){
+            expect(result.value.success).toBe(false);
+        } else {
+            fail('Result should be \'left\'');
+        }
+    })
+
+    it('should return UnexpectedError when saving fails', async () => {
+        const usecase = makeEditInfoUsecase({ 
+            ...deps,
+            save: Save.throw
+        });
+        const result = await usecase(props);
+        if(result.isLeft()){
+            expect(result.value instanceof AppErrors.UnexpectedError).toBe(true);
+            expect(result.value.success).toBe(false);
+        } else {
+            fail('Result should be \'left\'');
+        }
+    })
+})
