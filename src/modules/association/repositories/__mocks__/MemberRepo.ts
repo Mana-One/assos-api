07c525968962d8f6c71bc33b63723c9b6e795b83
Mana-One <paolo.manaois@yahoo.fr>
2021-07-07 02:25:25 +0200

    added new functions

--- src/modules/association/repositories/__mocks__/MemberRepo.ts
+++ src/modules/association/repositories/__mocks__/MemberRepo.ts
@@ -1,6 +1,37 @@
+import { UniqueId } from "../../../../core/domain";
+import { Role, UserEmail, UserName, UserPassword } from "../../../../shared/domain";
 import { Member } from "../../domain";
 
-export const Save = {
+
+const props = {
+    firstName: UserName.create("Paolo").getValue(),
+    lastName: UserName.create("Manaois").getValue(),
+    email: UserEmail.create("manager@yahoo.com").getValue(),
+    password: UserPassword.createHashed("azertyUIOP123$").getValue(),
+    role: Role.MANAGER
+};
+
+export const CountManagers = {
+    one: async (aId: string) => 1,
+    moreThanOne: async (aId: string) => 2,
+    throw: async (aID: string) => { throw new Error("oopsie"); }
+}
+
+export const FindMember = {
+    null: async (mId: string, aId:string) => null,
+    notNull: async (mId: string, aId:string) => {
+        return Member.create(
+            {
+                ...props,
+                associationId: new UniqueId(aId) 
+            }, 
+            new UniqueId(mId)
+        ).getValue();
+    },
+    throw: async (mId: string, aId:string) => { throw new Error("oopsie"); }
+}
+
+export const RemoveOrSave = {
     ok: async (m: Member) => {},
     throw: async (m: Member) => { throw new Error("oopsie"); }
 }
