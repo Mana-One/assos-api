27d6077c525ba4b3769f74e3879871061ed548fb
Mana-One <paolo.manaois@yahoo.fr>
2021-07-09 17:47:33 +0200

    added delete associaiton controller  + permissions

--- src/modules/association/infra/express/delete-association.controller.ts
+++ src/modules/association/infra/express/delete-association.controller.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express";
 import { UseCase } from "../../../../core/domain";
 import { ExpressController } from "../../../../core/infra";
 import { AppErrors, Guard } from "../../../../core/logic";
-import { isMemberRole, Role } from "../../../../shared/domain";
-import * as CreateMember from "../../usecases/CreateMember";
+import { Role } from "../../../../shared/domain";
+import * as DeleteAssociation from "../../usecases/DeleteAssociation";
 import { AssociationErrors } from "../../usecases/errors";
 
 
-export function makeMemberController(
-    usecase: UseCase<CreateMember.Input, Promise<CreateMember.Response>>
+export function makeDeleteAssociationController(
+    usecase: UseCase<DeleteAssociation.Input, Promise<DeleteAssociation.Response>>
 ){
     return async function(req: Request, res: Response){
         if(req.body.account?.role !== Role.MANAGER && req.body.account?.role !== Role.ADMIN){
@@ -16,14 +16,9 @@ export function makeMemberController(
         }
 
         const associationId = req.params.associationId;
-        const { firstName, lastName, email, password, role } = req.body;
-        const guard = Guard.bulkAgainstNullOrUndefined([
-            { key: 'associationId', value: associationId },
-            { key: 'firstName', value: firstName },
-            { key: 'lastName', value: lastName },
-            { key: 'email', value: email },
-            { key: 'password', value: password }
-        ]);
+        const guard = Guard.againstNullOrUndefined(
+            { key: 'associationId', value: associationId }
+        );
         if(!guard.success){
             return ExpressController.clientError(res, guard.message);
         }
@@ -33,27 +28,17 @@ export function makeMemberController(
             return ExpressController.forbidden(res);
         }
 
-        if(!isMemberRole(role)){
-            return ExpressController.clientError(res, 'Invalid role');
-        }
-
-        const result = await usecase({
-            associationId, firstName, lastName, email, password, role
-        });
+        const result = await usecase({ associationId });
         if(result.isRight()){
-            return ExpressController.created(res);
+            return ExpressController.noContent(res);
         }
 
         const error = result.value;
         switch(error.constructor){
             case AssociationErrors.AssociationNotFound:
                 return ExpressController.notFound(res, error.getValue().message);
-            case AssociationErrors.AccountAlreadyExists:
-                return ExpressController.conflict(res, error.getValue().message);
             case AppErrors.UnexpectedError:
                 return ExpressController.fail(res, error.getValue().message);
-            default:
-                return ExpressController.clientError(res, error.getValue());
         }
     }
 }
--- src/modules/association/infra/express/index.ts
+++ src/modules/association/infra/express/index.ts
@@ -1,1 +1,3 @@
 export * from './create-association.controller';
+export * from './create-member.controller';
+export * from './delete-association.controller';
