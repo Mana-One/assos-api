e1cfa4f83f39f52572f11a33a4cd1275e0d02e5b
Mana-One <paolo.manaois@yahoo.fr>
2021-07-14 03:19:03 +0200

    added functions

--- src/modules/article/repositories/__mocks__/ArticleWriteRepo.ts
+++ src/modules/article/repositories/__mocks__/ArticleWriteRepo.ts
@@ -1,4 +1,18 @@
+import { UniqueId } from "../../../../core/domain";
 import { Article } from "../../domain";
+
+export const FindById = {
+    null: async (articleId: string) => null,
+    notNull: async (articleId: string) => {
+        return Article.create({
+            title: 'a title',
+            content: 'a content',
+            publicationDate: new Date(),
+            associationId: new UniqueId('an association id')
+        }, new UniqueId(articleId)).getValue();
+    },
+    throw: async (articleId: string) => { throw new Error('oopsie'); }
+}
 
 export const Save = {
     ok: async (article: Article) => {},
--- src/modules/article/repositories/index.ts
+++ src/modules/article/repositories/index.ts
@@ -1,6 +1,10 @@
 import { Article } from "../domain";
 
 export namespace ArticleWriteRepo {
+    export interface FindById {
+        (articleId: string): Promise<Article | null>;
+    }
+
     export interface Save {
         (article: Article): Promise<void>;
     }
