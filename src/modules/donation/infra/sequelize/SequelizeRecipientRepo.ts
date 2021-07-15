f04548e210c6059f275a339c7cd6f5979aa39202
Mana-One <paolo.manaois@yahoo.fr>
2021-07-15 17:07:00 +0200

    quick fix

--- src/modules/donation/infra/sequelize/SequelizeRecipientRepo.ts
+++ src/modules/donation/infra/sequelize/SequelizeRecipientRepo.ts
@@ -2,12 +2,17 @@ import { Recipient } from "../../domain";
 import { RecipientRepo } from "../../repositories";
 import { models } from "../../../../infra/sequelize";
 import { RecipientMap } from "../../mappers";
+import { AssociationStatus } from "../../../association/domain";
 
 
 export namespace SequelizeRecipientRepo {
     export const findById: RecipientRepo.FindById = async (recipientId: string): Promise<Recipient | null> => {
-        const instance = await models.Association.findByPk(recipientId, {
-            attributes: ["id", "name", "storeReference"]
+        const instance = await models.Association.findOne({
+            attributes: ["id", "name", "storeReference"],
+            where: {
+                id: recipientId,
+                status: AssociationStatus.VALID
+            }
         });
         if(instance === null){
             return null;
