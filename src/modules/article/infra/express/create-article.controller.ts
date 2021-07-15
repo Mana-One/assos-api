07b68c31b1a3690a0fc4a878e9a56557ab9268b0
Mana-One <paolo.manaois@yahoo.fr>
2021-07-15 22:48:39 +0200

    added create article controller

---
+++ src/modules/article/infra/express/create-article.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { request } from "http";
+import { UseCase } from "../../../../core/domain";
+import { ExpressController } from "../../../../core/infra";
+import { AppErrors, Guard } from "../../../../core/logic";
+import { Role } from "../../../../shared/domain";
+import * as CreateArticle from "../../usecases/CreateArticle";
+
+
+export function makeCreateArticleController(
+    usecase: UseCase<CreateArticle.Input, Promise<CreateArticle.Response>>
+){
+    return async function(req: Request, res: Response){
+        if(req.body.account?.role !== Role.MANAGER){
+            return ExpressController.forbidden(res);
+        }
+
+        const associationId = req.body.account?.associationId;
+        const { title, content } = req.body;
+        const guard = Guard.bulkAgainstNullOrUndefined([
+            { key: 'associationId', value: associationId },
+            { key: 'title', value: title },
+            { key: 'content', value: content }
+        ]);
+        if(!guard.success){
+            return ExpressController.clientError(res, guard.message);
+        }
+
+        const result = await usecase({ title, content, associationId });
+        if(result.isRight()){
+            return ExpressController.created(res);
+        }
+
+        const error = result.value;
+        switch(error.constructor){
+            case AppErrors.UnexpectedError:
+                return ExpressController.fail(res, error.getValue().message);
+            default:
+                return ExpressController.clientError(res, error.getValue());
+        }
+    }
+}
