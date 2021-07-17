45cd5ccd0b2aad7b2cf822c9dc086be0847d69dd
Mana-One <paolo.manaois@yahoo.fr>
2021-07-17 21:47:51 +0200

    added socket controllers

---
+++ src/modules/message/infra/socket/load-more.controller.ts
@@ -0,0 +1,43 @@
+import { Socket } from "socket.io";
+import { UseCase } from "../../../../core/domain";
+import { AccessToken, Role } from "../../../../shared/domain";
+import { Authentication } from '../../../../shared/services';
+import * as ListMessages from '../../usecases/ListMessages';
+
+
+export interface LoadMoreInput {
+    token: AccessToken;
+    roomId: string;
+    offset: number;
+}
+
+export function makeLoadMoreController(
+    socket: Socket,
+    usecase: UseCase<ListMessages.Input, Promise<ListMessages.Response>>,
+    verifyToken: Authentication.VerifyAndRetrievePayload
+){
+    return socket.on('loadMore', async (input: LoadMoreInput) => {
+        const { token, roomId, offset } = input;
+        try {
+            const sender = await verifyToken(token);
+            if(sender.role === Role.MANAGER && 
+                sender.role !== Role.MANAGER && 
+                sender.associationId !== roomId){
+                socket.emit('error', 'Forbidden');
+                return;
+            }
+
+            const result = await usecase({ roomId, offset });
+            if(result.isRight()){
+                socket.emit('messages', result.value.getValue());
+                return;
+            }
+
+            const error = result.value;
+            socket.emit('error', error.getValue().message);
+
+        } catch(err) {
+            socket.emit('error', 'Forbidden');
+        }
+    });
+}
