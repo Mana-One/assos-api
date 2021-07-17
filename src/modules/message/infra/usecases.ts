6bb26aa4c4b9ecc8511d32045306c96ee7f618a7
Mana-One <paolo.manaois@yahoo.fr>
2021-07-17 21:50:23 +0200

    added usecase implementations

---
+++ src/modules/message/infra/usecases.ts
@@ -0,0 +1,12 @@
+import { makeListMessagesUsecase } from "../usecases/ListMessages";
+import { makePostMessageUsecase } from "../usecases/PostMessage";
+import { SequelizeMessageReadRepo, SequelizeMessageWriteRepo } from "./repositories";
+
+
+export const listMessagesUsecase = makeListMessagesUsecase({
+    listMessages: SequelizeMessageReadRepo.listByRoom
+});
+
+export const postMessage = makePostMessageUsecase({
+    save: SequelizeMessageWriteRepo.save
+});
